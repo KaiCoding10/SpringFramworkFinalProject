28a7dcba3f375f4cc55a1002064b10430a2eb0cd
KaiCoding10 <nguyennhatkien247@gmail.com>
2024-02-07 11:02:17 -0600

    Clone this project to GIthub

---
+++ src/main/java/com/example/springframeworkfinalproject/Model/Questions.java
@@ -0,0 +1,4 @@
+package com.example.springframeworkfinalproject.Model;
+
+public class Questions {
+}
---
+++ src/main/java/com/example/springframeworkfinalproject/TestRepository.java
@@ -0,0 +1,5 @@
+package com.example.springframeworkfinalproject;
+import com.example.springframeworkfinalproject.Model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+public interface TestRepository extends MongoRepository<User, String>{
+}
