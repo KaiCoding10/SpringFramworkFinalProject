d27b86193f43c3ee4aa5a5d39a00877540931b44
KaiCoding10 <nguyennhatkien247@gmail.com>
2024-02-08 10:26:21 -0600

    Create User model class
    Generate a table in the database called "user".

--- src/main/java/com/example/springframeworkfinalproject/Model/User.java
+++ src/main/java/com/example/springframeworkfinalproject/Model/User.java
@@ -1,4 +1,27 @@
 package com.example.springframeworkfinalproject.Model;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collation = "user")
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected int ID;
+
+    @Column
+    protected String firstName;
+
+    @Column
+    protected String lastName;
+    @Column
+    protected int score;
+
 }
