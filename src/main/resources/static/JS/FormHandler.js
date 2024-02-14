32d6f5f794081ff58f386ea3ee2da8c9c69fbeca
aa126237 <alexaanderson2002@gmail.com>
2024-02-14 10:21:00 -0600

    Javascript for Home Form
    dsadsa :)

---
+++ src/main/resources/static/JS/FormHandler.js
@@ -0,0 +1,29 @@
+const homeForm = document.querySelector("#homeForm");
+const saveButton = document.querySelector("submitBtn");
+
+saveButton.addEventListener("click", (e) => addUser(e));
+
+window.onload = () => {
+    fetch("http://localhost:8080/all")
+        .then((response) => response.json())
+};
+
+const addUser = (e) => {
+    e.preventDefault();
+    const user = {
+        fName: document.querySelector('#fName').value,
+        lName: document.querySelector('#lName').value,
+        id: document.querySelector('#id').value           //doesn't id auto increment?
+    }
+
+    fetch("http://localhost:8080/add", {
+        method: "POST",
+        headers: {"Content-Type" : "application/json"},
+        body: JSON.stringify(car)
+    })
+    .then((response) => response.text())
+    .then(() => window.location.reload());
+
+    //change :)
+    
+};
