cb085ed0b797a662e6cc6e665a972aaa5e38ee1b
aa126237 <alexaanderson2002@gmail.com>
2024-02-16 19:10:53 -0600

    small change

--- src/main/resources/static/JS/FormHandler.js
+++ src/main/resources/static/JS/FormHandler.js
@@ -21,7 +21,7 @@ const addUser = (e) => {
     fetch("http://localhost:8080/add", {
         method: "POST",
         headers: {"Content-Type" : "application/json"},
-        body: JSON.stringify(car)
+        body: JSON.stringify(user)
     })
     .then((response) => response.text())
     .then(() => window.location.reload());
